2decce4ee9e790bab1664df6b364b3cfbe3b1c30
GuilhermeObbet <ghcr23@gmail.com>
2023-12-26 18:26:58 -0300

    changes

--- JavaScript/Origamid/caderno-js.js
+++ JavaScript/Origamid/caderno-js.js
@@ -79,34 +79,34 @@
 //  img.forEach(function(item, index, array){
 //   console.log(item, index, array)
 // }); //mostra todos os itens detalhados, sempre tem que declarar na function o parametro que quer que mostre, exemplo acima. img.forEach(function(item, index, array){
-  //console.log(item, index, array)
+//console.log(item, index, array)
 
-  //ARROW FUNCTION
+//ARROW FUNCTION
 
-  //metodo normal
-  // const imgs = document.querySelectorAll('img');
-  // imgs.forEach(function(item){
-  //   console.log(item)
-  // });
+//metodo normal
+// const imgs = document.querySelectorAll('img');
+// imgs.forEach(function(item){
+//   console.log(item)
+// });
 
-  // //agora com ARROW FUNCTION
-  // const imgs = document.querySelectorAll('img');
-  // imgs.forEach((item) => {
-  //   console.log(item)
-  // }); // trocou o function pela seta.
-  
-  // Mostre no console cada parágrado do site
+// //agora com ARROW FUNCTION
+// const imgs = document.querySelectorAll('img');
+// imgs.forEach((item) => {
+//   console.log(item)
+// }); // trocou o function pela seta.
+
+// Mostre no console cada parágrado do site
 //   const todosParagrafos = document.querySelectorAll('p')
 //   todosParagrafos.forEach((paragrafos) => {
 //     console.log(paragrafos)
 //   });
-  
+
 //  //Mostre o texto dos parágrafos no console
 //  todosParagrafos.forEach((paragrafos) => {
 //   console.log(paragrafos.innerText)
 // });
 
- //Como corrigir os erros abaixo:
+//Como corrigir os erros abaixo:
 //  const imgs = document.querySelectorAll('img');
 
 //  imgs.forEach((item, index) => {
@@ -228,7 +228,7 @@
 // const imgDistance = document.querySelector('img');
 // imgDistance.getBoundingClientRect();
 // if (imgDistance.width <= 80) {
-//   console.log('vc está no jogo') 
+//   console.log('vc está no jogo')
 // }else  {
 //   console.log('vc excedeu o espaço')
 // }
@@ -244,7 +244,7 @@
 //   let soma = 0;
 //   imgsWidth.forEach((imgsWidth ) => {
 //   soma += imgsWidth.
-  
+
 //   console.log(soma);
 // }); //pode ter function dentro de function
 // }
@@ -285,9 +285,195 @@
 
 // menu.addEventListener('click', clicar)
 
-const menu = document.querySelector('.menu a')
-function clicar(event) {
-console.log(event)
-}
+// const menu = document.querySelector('.menu a')
+// function clicar(event) {
+// console.log(event)
+// }
 
-menu.addEventListener('click', clicar)
+// menu.addEventListener('click', clicar)
+
+// const img = document.querySelector('img');
+// img.addEventListener('click', function {
+
+// });
+
+// const img = document.querySelector('img');
+// function callback(event) {
+//   console.log(event);
+// }
+// // img.addEventListener('click', callback)
+
+// const animaisLista = document.querySelector('.animais-lista');
+// function callbackLista(event){
+//   console.log(event.currentTarget);
+//   console.log(event.target);
+// }
+
+// animaisLista.addEventListener('click', callbackLista);
+
+// links.forEach((link) => {
+//   const linkwidth = link.offsetWidth;
+//   const linkHeight = link.offsetHeight;
+//   console.log (link)
+// });
+
+// const linkExterno = document.querySelector('a[href^="http"]');
+
+// function podeSerQlqrNome() {
+//   console.log('Clicou');
+// }
+// linkExterno.addEventListener('click', podeSerQlqrNome);
+
+// function podeSerQlqrNome(event) {
+//   if (event.key === 'f') {
+//     document.body.classList.toggle('fullscreen')
+//   }
+// } // aqui podemos adcionar classes e muitas coisas somente clicando em uma tecla
+// window.addEventListener('keydown', podeSerQlqrNome);
+
+// const imgs = document.querySelectorAll('img');
+// function podeSerQlqrNome2(event) {
+//  console.log(event.target)
+// };
+// imgs.forEach((podeSerQlqrNome) => {
+//   podeSerQlqrNome.addEventListener('click', podeSerQlqrNome2)
+// });
+
+//  adicione a classe ativo ao item clicado e remova dos
+//  demais itens caso eles possuam a mesma. Previna
+//  Quando o usuário clicar nos links internos do site,
+//  o comportamento padrão desses links
+//  const linkInterno = document.querySelectorAll('a[href^="#"]');
+//  function aInterno(event) {
+//    event.preventDefault();
+//    linkInterno.forEach((link) => {
+//     link.classList.remove('ativo')
+//    });
+//    event.currentTarget.classList.add("ativo");
+//  }
+//  linkInterno.forEach((a) => {
+//    a.addEventListener("click", aInterno);
+//  })
+//  Selecione todos os elementos do site começando a partir do body,
+//  ao clique mostre exatamente quais elementos estão sendo clicados
+//  const corpo = document.querySelectorAll("body *");
+//  function qlqrNome (event) {
+//    console.log(event.target);
+//  }
+//  corpo.forEach((clicar) => {
+//  clicar.addEventListener("click", qlqrNome);
+//  });
+
+// Utilizando o código anterior, ao invés de mostrar no console,
+// remova o elemento que está sendo clicado, o método remove() remove um elemento
+// const remover = document.querySelectorAll("body");
+// function qlqrNome1 (event) {
+//   console.log(event.target.remove());
+// }
+// corpo.forEach((clicar) => {
+// clicar.addEventListener("click", qlqrNome1);
+// });
+// Se o usuário clicar na tecla (t), aumente todo o texto do site.
+//  function podeSerQlqrNome(event) {
+//      if (event.key === 't') {
+//       document.body.classList.toggle('hipertexto')
+//     }
+//   }
+//    window.addEventListener('keydown', podeSerQlqrNome);
+
+// const menu = document.querySelector('.menu');
+
+// menu.outerHTML; // todo o html do elemento
+// menu.innerHTML; // html interno
+// menu.innerText; // texto, sem tags
+
+// menu.innerText = '<p>Texto</p>'; // a tag vai como texto
+// menu.innerHTML = '<p>Texto</p>'; // a tag é renderizada
+
+// const lista = document.querySelector('.animais-lista');
+
+// lista.parentElement; // pai
+// lista.parentElement.parentElement; // pai do pai
+// lista.previousElementSibling; // elemento acima
+// lista.nextElementSibling; // elemento abaixo
+
+// lista.children; // HTMLCollection com os filhos
+// lista.children[0]; // primeiro filho
+// lista.children[--lista.children.length]; // último filho
+
+// lista.querySelectorAll('li'); // todas as LI's
+// lista.querySelector('li:last-child'); // último filho
+
+// const lista = document.querySelector('.animais-lista');
+
+// lista.previousElementSibling; // elemento acima
+// lista.previousSibling; // node acima
+
+// lista.firstChild; // primeiro node child
+// lista.childNodes; // todos os node child
+
+// const lista = document.querySelector('.animais-lista');
+// const contato = document.querySelector('.contato');
+// const titulo = contato.querySelector('.titulo');
+
+// contato.appendChild(lista); // move lista para o final de contato
+// contato.insertBefore(lista, titulo); // insere a lista antes de titulo, o inserido tem que ser filho do selecionado(titulo filho.contato).
+// contato.removeChild(titulo); // remove titulo de contato
+// contato.replaceChild(lista, titulo); // substitui titulo por lista
+
+// const animais = document.querySelector('.animais');
+
+// const novoH1 = document.createElement('h1');
+// novoH1.innerText = 'Novo Título';
+// novoH1.classList.add('titulo');
+
+// animais.appendChild(novoH1);
+
+// const titulo = document.querySelector('h1');
+// const titulo2 = document.querySelector('h1');
+// const novoTitulo = titulo;
+// // titulo, titulo2 e novoTitulo são iguais
+
+// const cloneTitulo = titulo.cloneNode(true);
+// const contato = document.querySelector('.contato');
+// contato.appendChild(cloneTitulo);
+
+// Duplique o menu e adicione ele em copy
+//   const menu = document.querySelector('.menu');
+//   const cloneMenu = menu.cloneNode(true);
+//   const copy = document.querySelector('.copy');
+
+//   copy.insertAdjacentElement('beforeend', cloneMenu);
+
+// // Selecione o primeiro DT da dl de Faq
+//   const primeiroFilho = document.querySelector('.faq-lista')
+//   console.log(primeiroFilho.children[0]);
+//   console.log(primeiroFilho.children[primeiroFilho.children.length-1]);
+
+// // Selecione o DD referente ao primeiro DT
+// console.log(primeiroFilho.children[0].nextElementSibling);
+// // Substitua o conteúdo html de .faq pelo de .animais
+
+// const tabMenu = document.querySelectorAll(".js-tabMenu li"); //li pra selecionar cada item.
+// const tabContain = document.querySelectorAll(".js-tabContain section"); // section para falar com as section.
+// if (tabMenu.length && tabContain.length) {
+//   tabContain[0].classList.add("ativo");
+
+//   function activeTab(itemFotos) {
+//     tabContain.forEach((contain) => {
+//       contain.classList.remove("ativo");
+//     });
+//     tabContain[itemFotos].classList.add("ativo");
+//   }
+
+//   tabMenu.forEach((itemMenu, itemFotos) => {
+//     itemMenu.addEventListener("click", () => {
+//       activeTab(itemFotos);
+//     });
+//   });
+// }
+
+
+
+
+ 
